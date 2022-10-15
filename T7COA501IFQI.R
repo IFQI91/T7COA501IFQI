1547310a2c1006662faaf2070352e85aac793add
IFQI91 <ifqi90791@gmail.com>
2022-10-14 21:32:40 -0500

    cambio

--- T7COA501IFQI.R
+++ T7COA501IFQI.R
@@ -1,3 +1,29 @@
 ##Tarea 7
 ##COA-501 Herramientas de cómputo para investigadores (R y Python)
 ## Alumno: Ivan Fermin Quiroz Ibañez
+
+#1. Con los los archivos de datos adjuntos en Excel.
+
+#a) Cree archivos con formato .csv para su lectura en R.
+#b) Con el archivo 1 calcule la media ponderada de calificaciones por matrícula.
+#c) Realice un empalme (merge) de los datos por id-est y cree un objeto data.frame con todas las
+#variables, incluyendo la columna de calif_ponderada.
+#d) Exporte el objeto creado a un archivo tipo csv.
+#e) Con el archivo 2 de datos de coeficiente intelectual y producto interno bruto, cree un objeto
+#data.frame con todos los datos, incluyendo los valores perdidos; luego, remplace los valores
+#perdidos con la mediana de los datos correspondientes a la columna donde aparecen los datos
+#perdidos.
+#f)Explore los datos entre PIB e IQ
+#g) Efectúe una regresión lineal entre PIB (var dependiente) e IQ (var independiente)
+#h) Verifique la normalidad de los residuales del modelo.
+#i)Elabore una gráfica para comparar el modelo estimado con los datos observados
+#j)Luego realice el mismo análisis realizando una transformación logarítmica de ambas
+#variables; esto es, ln(PIB) vs ln(IQ)
+#k) Verifique la normalidad de los residuos del modelo.
+#l)Analice e interprete sus resultados (traduzca previamente, el texto en inglés de la
+#descripción de los datos).
+
+#2. Con base en el script ACP visto en clase, corra el script con los datos de wine.datos adjunto. Cree
+#un archivo csv. consulte el link señalado para hacer una breve síntesis en español de la aplicación
+#del Análisis de componentes principales para reducir dimensionalidad e introduzca comentarios
+#al script. Elabore las gráficas indicadas con etiquetas en español.
